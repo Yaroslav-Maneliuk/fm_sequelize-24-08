e593ce3160d59a2ccbca95262e516c1e65720e01
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-08-30 12:05:52 +0300

    Add image and more

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -1,8 +1,29 @@
-const {Router} = require("express")
-const GroupController = require("../controllers/group.controller")
-const groupRouter = Router()
+const path = require("path");
+const { Router } = require("express");
+const multer = require("multer");
 
-groupRouter.post('/', GroupController.createGroupByUser)
+const GroupController = require("../controllers/group.controller");
+const groupRouter = Router();
 
+// const upload = multer({ dest: path.resolve(__dirname, "../public/images") });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, "../public/images"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+groupRouter.post("/", GroupController.createGroupByUser);
+groupRouter.get("/users/:userId", GroupController.getAllGroupsByUser);
+groupRouter.post(
+  "/:groupId/image",
+  upload.single("image"),
+  GroupController.createImageToGroup
+);
 
 module.exports = groupRouter;
